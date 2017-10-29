844a57612ee65b4d271aadc3bdf9a13b211a7e22
jimako <nsplat@gmail.com>
2017-10-29 12:58:03 +0900

    Modified Dockerfile to revert the entry dir.

--- docker_run.sh
+++ docker_run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
-docker run -v ${SCRIPT_DIR}/home:/home/toybox -p 127.0.0.1:5900:5900 -p 127.0.0.1:5901:5901 -e JAPANESE_SUPPORT=yes -w /home/toybox -itd jimako1989/centos-vnc_scapy3
+THIS_SCRIPT_DIR=$(cd $(dirname $0); pwd)
+CONTAINER_LOGON_DIR=/root
+docker run -v ${THIS_SCRIPT_DIR}/home:${CONTAINER_LOGON_DIR} -p 127.0.0.1:5900:5900 -p 127.0.0.1:5901:5901 -e JAPANESE_SUPPORT=yes -w ${CONTAINER_LOGON_DIR} -itd jimako1989/centos-vnc_scapy3
